bfe946e5686ff6ab5d3775570b6bbf6e80f28e07
Jason McGinty <jason_mcginty@yahoo.com>
2021-08-25 20:00:41 -0500

    removing account key

--- blobServiceClient.js
+++ blobServiceClient.js
@@ -1,4 +1,6 @@
 const { BlobServiceClient } = require('@azure/storage-blob');
-const CONNECTION_STRING = 'DefaultEndpointsProtocol=https;AccountName=ironradiostorage;AccountKey=7Yjw0GHBQPdNuEjydL9C4pOZYQZHt9ueCOPhnElSWamdC0eJBF1N5dEMQ/KCQERTXalAYXQ6VULw5ci+6CdJHg==;EndpointSuffix=core.windows.net';
+const ACCOUNT_NAME = process.env.ACCOUNT_NAME;
+const ACCOUNT_KEY = process.env.ACCOUNT_KEY;
+const CONNECTION_STRING = `DefaultEndpointsProtocol=https;AccountName=${ACCOUNT_NAME};AccountKey=${ACCOUNT_KEY};EndpointSuffix=core.windows.net`;
 
 module.exports = BlobServiceClient.fromConnectionString(CONNECTION_STRING);
